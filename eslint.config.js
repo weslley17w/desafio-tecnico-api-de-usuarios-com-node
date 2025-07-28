cadbdfcd88bb3357c83f05c0d9536d2a9b4e7fec
Weslley Barros <weslley17w@gmail.com>
2025-07-28 15:34:12 -0300

    test: add Jest setup and initial configuration for testing

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,12 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import json from '@eslint/json';
+import pluginJest from 'eslint-plugin-jest';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
+  { files: ['**/*.spec.{js,mjs,cjs,ts,mts,cts}'], plugins: { jest: pluginJest }, languageOptions: { globals: pluginJest.configs.recommended.globals } },
+  { files: ['**/*.spec.{js,mjs,cjs,ts,mts,cts}'], extends: ['plugin:jest/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   tseslint.configs.recommended,
